e95a19797d488d683769161e213a6fec20b69f5b
Andrekcar <126022147+Andrekcar@users.noreply.github.com>
2026-04-03 15:46:23 +0000

    Creacion de la tabla emprendedores en coordinacion con Yoly

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Emprendedor(models.Model):
+    nombre = models.CharField(max_length=50)
+    tipo_documento = models.CharField(max_length=2, choices=[('CC', 'Cédula de Ciudadanía'), ('CE', 'Cédula de Extranjería')])
+    documento = models.CharField(max_length=15, unique=True)
+    actividad_economica = models.CharField(max_length=100)
+    sector = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    direccion = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+
+
+
