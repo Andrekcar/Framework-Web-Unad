c945dae77db90345ae8d64b4256ec6bc56afa0f0
Andrekcar <126022147+Andrekcar@users.noreply.github.com>
2026-04-13 20:38:16 +0000

    Ultimos ajustes

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,36 @@
 from django.contrib import admin
-
-# Register your models here.
-
+from django.utils.html import format_html
+from django.urls import reverse
 from .models import Emprendedor
 
 @admin.register(Emprendedor)
 class EmprendedorAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'tipo_documento', 'documento', 'actividad_economica', 'sector', 'telefono', 'email', 'direccion')
-    editable_fields = ('nombre', 'tipo_documento', 'documento', 'actividad_economica', 'sector', 'telefono', 'email', 'direccion')
+    list_display = ('nombre', 'actividad_economica', 'sector', 'acciones')
     search_fields = ('nombre', 'documento')
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                ('nombre', 'tipo_documento', 'documento'),
+                ('actividad_economica', 'sector'),
+                ('telefono', 'email'),
+                'direccion',
+            )
+        }),
+    )
+
+    def acciones(self, obj):
+        edit = reverse('admin:core_emprendedor_change', args=[obj.pk])
+        delete = reverse('admin:core_emprendedor_delete', args=[obj.pk])
+        return format_html(
+            '<div>'
+            '<a href="{}" title="Ver detalles">?</a>'
+            '<a href="{}" title="Editar">&#9998;</a>'
+            '<a href="{}" title="Eliminar">&#10005;</a>'
+            '</div>',
+            edit, edit, delete
+        )
+
+admin.site.enable_nav_sidebar = False
+   
+
