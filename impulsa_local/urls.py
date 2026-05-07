f5b2c0ab3cbf24314c16d3339525bed7bb7e0bc1
Andrekcar <126022147+Andrekcar@users.noreply.github.com>
2026-05-07 00:24:44 +0000

    Ajuste url

--- impulsa_local/urls.py
+++ impulsa_local/urls.py
@@ -18,8 +18,8 @@ Including another URLconf
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 from core.forms import EmprendedorPasswordResetForm
-from core.views import home, inscribir
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +29,5 @@ urlpatterns = [
         name="password_reset",
     ),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("", home, name="home"),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
