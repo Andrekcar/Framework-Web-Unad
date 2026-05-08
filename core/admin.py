a3b1fef058c4f3c9fe139cceeab13fa5b99ad33e
Andrekcar <126022147+Andrekcar@users.noreply.github.com>
2026-05-08 23:01:48 +0000

    nuevo apartado vista admin

--- core/admin.py
+++ core/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from django.urls import reverse
-from .models import Emprendedor, Programas
+from .models import Emprendedor, Programas, observaciones
 
 @admin.register(Programas)
 class ProgramasAdmin(admin.ModelAdmin):
     change_list_template = 'admin/core/programas/form.html'
     list_display = ('nombre', 'fecha_inicio', 'fecha_fin', 'cupos')
 
+
 @admin.register(Emprendedor)
 class EmprendedorAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'actividad', 'sector', 'acciones')
+    list_display = ('nombre', 'actividad', 'sector', 'programa', 'acciones')
     search_fields = ('nombre', 'documento')
 
     fieldsets = (
@@ -35,6 +36,20 @@ class EmprendedorAdmin(admin.ModelAdmin):
             '</div>',
             edit, edit, delete
         )
+
+@admin.register(observaciones)
+class ObservacionesAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/core/emprendedor/change_obs_form.html'
+    list_display = ('emprendedor', 'programa', 'fecha', 'observacion')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 admin.site.enable_nav_sidebar = False
    
